4318cf2596c903438f0022df9abfee91d1c25e29
José Otávio Junqueira Ramos <167153159+JoseOtavioJunqueira@users.noreply.github.com>
2025-10-06 13:02:56 -0300

    Update GlobalExceptionHandler.java

--- src/main/java/com/itau/desafio/transacaoestatisticaapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itau/desafio/transacaoestatisticaapi/exception/GlobalExceptionHandler.java
@@ -1,24 +1,24 @@
-// // src/main/java/com/itau/desafio/transacaoestatisticaapi/exception/GlobalExceptionHandler.java
-// package com.itau.desafio.transacaoestatisticaapi.exception;
+package com.itau.desafio.transacaoestatisticaapi.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.MethodArgumentNotValidException;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
+@Hidden
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(MethodArgumentNotValidException.class)
-//     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//         // Quando uma validação falha, o status correto é 422
-//         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
-//     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
     
-//     @ExceptionHandler(com.fasterxml.jackson.databind.exc.InvalidFormatException.class)
-//     public ResponseEntity<Object> handleInvalidFormat(com.fasterxml.jackson.databind.exc.InvalidFormatException ex) {
-//         // Quando o formato do JSON está incorreto (ex: texto no lugar de número)
-//         return ResponseEntity.badRequest().build();
-//     }
-// }
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex) {
+        return ResponseEntity.badRequest().build();
+    }
+}
